8550c5b4fb07e2cec3cbf08aaf90d7f2a16d76ec
robcio89 <robcio89@gmail.com>
2015-11-26 21:43:56 +0100

    working and tested WorldPlay

--- UnitTest/WordPlayTest.java
+++ UnitTest/WordPlayTest.java
@@ -8,6 +8,72 @@ public class WordPlayTest extends TestCase {
 	public void testIsVowel() throws Exception {
 		WordPlay w = new WordPlay();
 
-		assertTrue(w.isVowel('g'));
+		assertTrue(w.isVowel('a'));
+
+		assertTrue(w.isVowel('A'));
+
+		assertFalse(w.isVowel('g'));
+		assertFalse(w.isVowel('H'));
+	}
+
+	public void testReplaceVowels() throws Exception {
+		WordPlay w = new WordPlay();
+
+		String modified = "HgllgWgrld";
+
+		assertEquals(modified, w.replaceVowels("HelloWorld", 'g'));
+	}
+
+	public void testReplaceVowels1() throws Exception {
+		WordPlay w = new WordPlay();
+
+		String modified = "*l*m*k*t*";
+
+		assertEquals(modified, w.replaceVowels("Alamakota", '*'));
+	}
+
+	public void testReplaceVowels2() throws Exception
+	{
+		WordPlay w = new WordPlay();
+
+		String modified = "#br#k#d#br#";
+
+		assertEquals(modified, w.replaceVowels("AbrAkadAbra", '#'));
+	}
+
+	public void testEmphasize() throws Exception
+	{
+		WordPlay w = new WordPlay();
+
+		String modified = "dn* ctg+*+ctg+";
+
+		assertEquals(modified, w.emphasize("dna ctgaaactga", 'a'));
+	}
+
+	public void testEmphasize1() throws Exception
+	{
+		WordPlay w = new WordPlay();
+
+		String modified = "M+ry Bell+ +br*c*d*br+";
+
+		assertEquals(modified, w.emphasize("Mary Bella Abracadabra", 'a'));
+	}
+
+	public void testEmphasize3() throws Exception
+	{
+		WordPlay w = new WordPlay();
+
+		String modified = "dn* ctg+*+ctg+";
+
+		assertEquals(modified, w.emphasize("dna ctgaaactga", 'A'));
+	}
+
+	public void testEmphasize4() throws Exception
+	{
+		WordPlay w = new WordPlay();
+
+		String modified = "M+ry Bell+ +br*c*d*br+";
+
+		assertEquals(modified, w.emphasize("Mary Bella Abracadabra", 'A'));
 	}
 }
--- src/WordPlay.java
+++ src/WordPlay.java
@@ -1,10 +1,48 @@
+import java.util.Arrays;
+
 /**
  * Created by Robert on 2015-11-24.
  */
 public class WordPlay {
 
-	public boolean isVowel(char ch)
+	private Character[] vowels = {'a', 'e', 'i', 'o', 'u','y','A','I', 'E', 'O', 'U','Y'};
+
+	public boolean isVowel(Character ch)
 	{
-		return true;
+		if (Arrays.asList(vowels).contains(ch))
+			return true;
+		return false;
+	}
+
+	public String replaceVowels(String phrase, Character ch)
+	{
+		String ret = phrase;
+		for (Character c: phrase.toCharArray())
+		{
+			if (isVowel(c))
+			{
+				ret = ret.replace(c, ch);
+			}
+		}
+		return ret.toString();
+	}
+
+	public String emphasize(String phrase, Character ch)
+	{
+		StringBuilder ret = new StringBuilder(phrase);
+
+		for (int i = 0; i < ret.length(); i++)
+		{
+			Character newChar = ret.charAt(i);
+			if (i % 2 != 0 && Character.toLowerCase(newChar) == Character.toLowerCase(ch))
+			{
+				ret.setCharAt(i, '+');
+			}
+			else if (i % 2 == 0 && Character.toLowerCase(newChar) == Character.toLowerCase(ch))
+			{
+				ret.setCharAt(i, '*');
+			}
+		}
+		return ret.toString();
 	}
 }
